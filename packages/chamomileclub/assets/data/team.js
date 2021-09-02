49528d3ff5aa293b8e9d11d63753fde65f862b4d
Fidel Elie <felie@tabernacleschool.co.uk>
2021-09-02 17:00:17 +0100

    Added Team Member Vincci

--- packages/chamomileclub/assets/data/team.js
+++ packages/chamomileclub/assets/data/team.js
@@ -10,6 +10,12 @@ const team = [
     position: "Venue Owner",
     text: "A cheerful player form the start of the night. But, when the shades come on and the smiles dissipate you better be ready. A different beast is born. Scary, unpredicatable and hungry... For you chips. Proceed with caution.",
     image: "/images/team/khanda.jpeg"
+  },
+  {
+    name: "Vincci",
+    position: "Guest Chef",
+    text: "New to the poker scene, but no less hungry to take the win. Finishing her stint in Paris, she is ready to pick up the French deck and make everyone wish she never came back. Their money would have been safer that way.",
+    image: "/images/team/vincci.jpeg"
   }
 ]
 
