ac0babe55f43a89658a79b7690b5b137acbe3000
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2022-01-06 17:38:35 +0000

    Conversion to typescript

---
+++ packages/chamomileclub/pages/_app.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+import type { AppProps } from "next/app"
+import "./_app.css";
+
+// ! Library
+import ThemeProvider, { useTheme } from "../lib/providers/theme";
+
+function TheChamomileClub({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+      <AppContainer>
+        <Component {...pageProps} />
+      </AppContainer>
+    </ThemeProvider>
+  )
+}
+
+const AppContainer = (props: { children: ReactNode }) => {
+  const { children } = props;
+  const theme = useTheme()[0];
+  return (
+    <div className={theme}>
+      { children }
+    </div>
+  )
+}
+
+export default TheChamomileClub;
